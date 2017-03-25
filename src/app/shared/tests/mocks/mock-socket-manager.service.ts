94b883edf3243357b052ad892db03d1c9b506321
Adrien de Peretti <adrien.deperetti@dougs.fr>
2017-03-25 11:03:12 +0100

    remove import

--- src/app/shared/tests/mocks/mock-socket-manager.service.ts
+++ src/app/shared/tests/mocks/mock-socket-manager.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/Observable/of';
 
 @Injectable()
 export class MockSocketManagerService {
